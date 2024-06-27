ab15f1d86b1ee5aa474c3d82fe6e6a6631ce5415
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-06-27 20:37:54 +0300

    Update Grid

--- src/components/grid_points.jsx
+++ src/components/grid_points.jsx
@@ -28,110 +28,110 @@ function GridBox() {
         </p>
       </div>
       <div className="grid md:grid-cols-5 md:grid-rows-2 md:px-32 mt-20">
-        <div className="px-5 py-10 border border-stone-600   flex flex-col justify-between text-center h-64 relative bg-transparent hover:scale-110 duration-200 hover:bg-background hover:z-10">
+        <div className="px-5 py-10 bg-background border border-stone-600   flex flex-col justify-between text-center h-64 relative  hover:scale-110 duration-200 hover:bg-background hover:shadow-lg hover:z-10">
           
           <FontAwesomeIcon icon={faDollarSign} className="text-5xl text-green"/>
           <div>
-          <h3 className="text-white font-inter font-black text-xl">Cost Reduction</h3>
+          <h3 className="text-white font-inter font-black text-md">Cost Reduction</h3>
           <p className="text-stone-400 font-inter text-xs">
             Identify and eliminate inefficiencies, cutting operational costs
             significantly.
           </p>
           </div>
         </div>
-        <div className="px-5 py-10 border border-stone-600   flex flex-col justify-between text-center h-64 relative bg-transparent hover:scale-110 duration-200 hover:bg-background hover:z-10">
+        <div className="px-5 py-10 bg-background border border-stone-600   flex flex-col justify-between text-center h-64 relative  hover:scale-110 duration-200 hover:bg-background hover:shadow-lg hover:z-10">
         
           <FontAwesomeIcon icon={faChartLine} className="text-5xl text-green" />
           <div>
-          <h3 className="text-white font-inter font-black text-xl">Profitability Boost</h3>
+          <h3 className="text-white font-inter font-black text-md">Profitability Boost</h3>
           <p className="text-stone-400 font-inter text-xs">
             Streamline processes to enhance productivity and increase your
             bottom line.
           </p>
           </div>
         </div>
-        <div className="px-5 py-10 border border-stone-600   flex flex-col justify-between text-center h-64 relative bg-transparent hover:scale-110 duration-200 hover:bg-background hover:z-10">
+        <div className="px-5 py-10 bg-background border border-stone-600   flex flex-col justify-between text-center h-64 relative  hover:scale-110 duration-200 hover:bg-background hover:shadow-lg hover:z-10">
         
           <FontAwesomeIcon icon={faCog} className="text-5xl text-green" />
           <div>
-          <h3 className="text-white font-inter font-black text-xl">Enhanced Efficiency</h3>
+          <h3 className="text-white font-inter font-black text-md">Enhanced Efficiency</h3>
           <p className="text-stone-400 font-inter text-xs"> 
             Optimize every aspect of your business for maximum performance and
             minimal waste.
           </p>
           </div>
         </div>
-        <div className="px-5 py-10 border border-stone-600   flex flex-col justify-between text-center h-64 relative bg-transparent hover:scale-110 duration-200 hover:bg-background hover:z-10">
+        <div className="px-5 py-10 bg-background border border-stone-600   flex flex-col justify-between text-center h-64 relative  hover:scale-110 duration-200 hover:bg-background hover:shadow-lg hover:z-10">
         
           <FontAwesomeIcon icon={faChartBar} className="text-5xl text-green" />
           <div>
-          <h3 className="text-white font-inter font-black text-xl">Real-Time Insights</h3>
+          <h3 className="text-white font-inter font-black text-md">Real-Time Insights</h3>
           <p className="text-stone-400 font-inter text-xs">
             Gain instant access to data-driven insights for smarter, quicker
             decisions.
           </p>
           </div>
         </div>
-        <div className="px-5 py-10 border border-stone-600   flex flex-col justify-between text-center h-64 relative bg-transparent hover:scale-110 duration-200 hover:bg-background hover:z-10">
+        <div className="px-5 py-10 bg-background border border-stone-600   flex flex-col justify-between text-center h-64 relative  hover:scale-110 duration-200 hover:bg-background hover:shadow-lg hover:z-10">
         
           <FontAwesomeIcon icon={faArrowsAlt} className="text-5xl text-green" />
           <div>
-          <h3 className="text-white font-inter font-black text-xl">Scalable Solutions</h3>
+          <h3 className="text-white font-inter font-black text-md">Scalable Solutions</h3>
           <p className="text-stone-400 font-inter text-xs">
             Adapt the simulation to match the complexity and size of your
             operations.
           </p>
           </div>
         </div>
-        <div className="px-5 py-10 border border-stone-600  flex flex-col justify-between text-center h-64 relative bg-transparent hover:scale-110 duration-200 hover:bg-background hover:z-10">
+        <div className="px-5 py-10 bg-background border border-stone-600  flex flex-col justify-between text-center h-64 relative  hover:scale-110 duration-200 hover:bg-background hover:shadow-lg hover:z-10">
         
           <FontAwesomeIcon icon={faFlask} className="text-5xl text-green" />
           <div>
-          <h3 className="text-white font-inter font-black text-xl">Risk-Free Testing</h3>
+          <h3 className="text-white font-inter font-black text-md">Risk-Free Testing</h3>
           <p className="text-stone-400 font-inter text-xs">
             Experiment with scenarios in a virtual environment to ensure success
             before real-world implementation.
           </p>
           </div>
         </div>
-        <div className="px-5 py-10 border border-stone-600   flex flex-col justify-between text-center h-64 relative bg-transparent hover:scale-110 duration-200 hover:bg-background hover:z-10">
+        <div className="px-5 py-10 bg-background border border-stone-600   flex flex-col justify-between text-center h-64 relative  hover:scale-110 duration-200 hover:bg-background hover:shadow-lg hover:z-10">
         
           <FontAwesomeIcon icon={faProjectDiagram} className="text-5xl text-green" />
           <div>
-          <h3 className="text-white font-inter font-black text-xl">Advanced Analytics</h3>
+          <h3 className="text-white font-inter font-black text-md">Advanced Analytics</h3>
           <p className="text-stone-400 font-inter text-xs">
             Leverage predictive and prescriptive analytics for better strategic
             planning.
           </p>
           </div>
         </div>
-        <div className="px-5 py-10 border border-stone-600   flex flex-col justify-between text-center h-64 relative bg-transparent hover:scale-110 duration-200 hover:bg-background hover:z-10">
+        <div className="px-5 py-10 bg-background border border-stone-600   flex flex-col justify-between text-center h-64 relative  hover:scale-110 duration-200 hover:bg-background hover:shadow-lg hover:z-10">
         
           <FontAwesomeIcon icon={faCogs} className="text-5xl text-green" />
           <div>
-          <h3 className="text-white font-inter font-black text-xl">Customizable Models</h3>
+          <h3 className="text-white font-inter font-black text-md">Customizable Models</h3>
           <p className="text-stone-400 font-inter text-xs">
             Tailor simulations to meet your specific business needs and
             objectives.
           </p>
           </div>
         </div>
-        <div className="px-5 py-10 border border-stone-600   flex flex-col justify-between text-center h-64 relative bg-transparent hover:scale-110 duration-200 hover:bg-background hover:z-10">
+        <div className="px-5 py-10 bg-background border border-stone-600   flex flex-col justify-between text-center h-64 relative  hover:scale-110 duration-200 hover:bg-background hover:shadow-lg hover:z-10">
         
           <FontAwesomeIcon icon={faHandsHelping} className="text-5xl text-green" />
           <div>
-          <h3 className="text-white font-inter font-black text-xl">Expert Support</h3>
+          <h3 className="text-white font-inter font-black text-md">Expert Support</h3>
           <p className="text-stone-400 font-inter text-xs">
             Benefit from the guidance of seasoned engineers dedicated to
             optimizing your operations.
           </p>
           </div>
         </div>
-        <div className="px-5 py-10 border border-stone-600   flex flex-col justify-between text-center h-64 relative bg-transparent hover:scale-110 duration-200 hover:bg-background hover:z-10">
+        <div className="px-5 py-10 bg-background border border-stone-600   flex flex-col justify-between text-center h-64 relative  hover:scale-110 duration-200 hover:bg-background hover:shadow-lg hover:z-10">
         
           <FontAwesomeIcon icon={faPuzzlePiece} className="text-5xl text-green" />
           <div>
-          <h3 className="text-white font-inter font-black text-xl">Seamless Integration</h3>
+          <h3 className="text-white font-inter font-black text-md">Seamless Integration</h3>
           <p className="text-stone-400 font-inter text-xs">
             Ensure smooth compatibility with your existing systems and
             processes.
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -115,10 +115,11 @@ function LandingPage() {
         </div>
       </div>
       <Services />
-      <div className="flex flex-col justify-center md:items-center sm:gap-4 md:gap-4 md:my-20 sm:mx-5 h-screen">
-        <p className="text-green font-inter font-bold md:text-center text-left">Success isn't a secret; it's a strategy…</p>
-        <h1 className="text-white font-inter font-black md:text-headline-2 text-2xl md:w-3/5 md:text-center leading-normal">The Fastest And Easiest Way To Guarantee Success!</h1>
-        <p className="text-white font-inter md:text-center md:w-2/3 md:mt-2 leading-relaxed">As a business leader, you understand the importance of swiftly boosting profits and cutting costs. After all, time is money, and poor operations drain your business's resources. Luckily, there are a few solutions. You can either gamble away with analyzing charts, risk testing in the real world for months, hope to get things magically right on the first go, or take a better route… </p>
+      <div className="flex flex-col justify-center md:items-start sm:gap-4 md:gap-4 md:my-20 mx-5 md:px-32 h-screen">
+        <div className="absolute md:w-64 md:h-64 bg-emerald-400 z-0 rounded-full blur-3xl animate-pulse"></div>
+        <p className="text-green font-inter font-bold text-left z-10">Success isn't a secret; it's a strategy…</p>
+        <h1 className="text-white font-inter font-black md:text-headline-2 text-2xl md:w-3/5 leading-normal z-10">The Fastest And Easiest Way To Guarantee Success!</h1>
+        <p className="text-white font-inter md:w-2/3 md:mt-2 leading-relaxed z-10">As a business leader, you understand the importance of swiftly boosting profits and cutting costs. After all, time is money, and poor operations drain your business's resources. Luckily, there are a few solutions. You can either gamble away with analyzing charts, risk testing in the real world for months, hope to get things magically right on the first go, or take a better route… </p>
       </div>
       <GridBox/>
     </>
