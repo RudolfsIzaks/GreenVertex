f94ae879ac721d3570e71f746417e1dc31d776db
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-07-05 23:05:08 +0200

    tyjhgytdrsd

--- src/components/booking.jsx
+++ src/components/booking.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import '../index.css';
 
 function Booking() {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = "https://app.reclaim.ai/scripts/embed-scheduling-link.0.x.x.js";
+        script.setAttribute('data-id', '52e479f8-bde6-4d65-b989-c042334275e6');
+        script.setAttribute('data-redirect', 'NONE');
+        script.async = true;
 
-    return(
-        <>
-         <div><script src="https://app.reclaim.ai/scripts/embed-scheduling-link.0.x.x.js" data-id="52e479f8-bde6-4d65-b989-c042334275e6" data-redirect="NONE"></script></div>
-        </>
-    )
+        document.body.appendChild(script);
+
+        // Cleanup function to remove the script when the component unmounts
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    return (
+        <div id="booking-container"></div>
+    );
 }
 
 export default Booking;
